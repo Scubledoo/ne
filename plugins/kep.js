e7b0c2cbced1d5a9bb6517aa7c627cc588968ca4
Scubledoo <101173548+Scubledoo@users.noreply.github.com>
2022-07-12 14:06:48 +0800

    Update kep.js

--- plugins/kep.js
+++ plugins/kep.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn }) => {
-let kasar = fs.readFileSync('./mp3/.WhatsApp-Audio-2021-04-01-at-14.11.16.opus') 
+let kasar = fs.readFileSync('./mp3/WhatsApp-Audio-2021-04-01-at-14.11.16.opus') 
 conn.sendFile(m.chat, kasar, '', '', m, true)
 }
 
